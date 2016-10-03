62b03879fb7752d2c67edcced6c4850bbf50b5a5
Francisco José García García <frangarcj@gmail.com>
2016-10-03 10:56:39 +0000

    First Try

--- build_common.sh
+++ build_common.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
 
-HOST_NATIVE=x86_64-apple-darwin10
+# Set variables according to real environment to make this script can run
+# on Ubuntu and Mac OS X.
+uname_string=`uname | sed 'y/LINUXDARWINFREEOPENPCBSD/linuxdarwinfreeopenpcbsd/'`
+host_arch=`uname -m | sed 'y/XI/xi/'`
+if [ "x$uname_string" == "xlinux" ] ; then
+    HOST_NATIVE="$host_arch"-linux-gnu
+    PACKAGE_NAME_SUFFIX=linux
+    GCC_CONFIG_OPTS_LCPP="--with-host-libstdcxx=-static-libgcc -Wl,-Bstatic,-lstdc++,-Bdynamic -lm"
+    JOBS=-j`grep ^processor /proc/cpuinfo|wc -l`
+elif [ "x$uname_string" == "xdarwin" ] ; then
+    HOST_NATIVE=x86_64-apple-darwin10
+    PACKAGE_NAME_SUFFIX=mac
+    GCC_CONFIG_OPTS_LCPP="--with-host-libstdcxx=-static-libgcc -Wl,-lstdc++ -lm"
+    JOBS=-j4
+else
+    error "Unsupported build system : $uname_string"
+fi
 
 LIBICONV_VERSION=1.14
 GMP_VERSION=6.1.1
@@ -15,8 +31,6 @@ DLFCN_VERSION=1.0.0
 BINUTILS_VERSION=2.27
 GCC_VERSION=6.2.0
 
-JOBS=-j4
-
 ROOTDIR=`pwd`
 PATCHDIR=${ROOTDIR}/patch
 DOWNLOADDIR=${ROOTDIR}/download
@@ -29,7 +43,7 @@ INSTALLDIR=${ROOTDIR}/install
 VITASDKROOT=${ROOTDIR}/vitasdk
 RELEASEDATE=`date +%Y%m%d`
 PACKAGE_NAME=gcc-arm-vita-eabi-6.2-${RELEASEDATE}
-PACKAGE_NAME_NATIVE=${PACKAGE_NAME}-mac
+PACKAGE_NAME_NATIVE=${PACKAGE_NAME}-${PACKAGE_NAME_SUFFIX}
 
 
 # Strip binary files as in "strip binary" form, for both native(linux/mac) and mingw.
